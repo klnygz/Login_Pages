732a10457e51bae3b957790b50677511c0f71c7d
Kullanıcı <Kullanc@DESKTOP-QJKFBU2>
2025-06-19 16:49:18 +0300

    update

--- Login_Pages_UI/Controllers/MemberController.cs
+++ Login_Pages_UI/Controllers/MemberController.cs
@@ -1,9 +1,11 @@
 ﻿using Entities.Entity;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Login_Pages_UI.Controllers
 {
+    [Authorize]
     public class MemberController : Controller
     {
         private readonly SignInManager<AppUser> _signInManager;
@@ -11,6 +13,11 @@ namespace Login_Pages_UI.Controllers
         public MemberController(SignInManager<AppUser> signInManager)
         {
             _signInManager = signInManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
         }
 
         public async Task<IActionResult> Logout()
